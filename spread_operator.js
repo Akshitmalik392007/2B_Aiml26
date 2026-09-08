a531481a358f016902e1cc244df4eafa09ed220e
Akshitmalik392007 <only4akshit@gmail.com>
2026-09-08 12:15:43 +0530

    spread operator concept

---
+++ spread_operator.js
@@ -0,0 +1,71 @@
+// function sum(...values){
+//     let s=0
+//     for(let i of values){
+//         s+=i
+
+//     }
+//     console.log(s)
+// };
+// let arr=[10,20,30,40]
+// sum(...arr)
+
+
+// let a = [10,20,30,40]
+// let b = [40,50,60,70,80]
+// let c = [...a,100,...b]
+
+// console.log(c);
+
+
+// let name ="Akshit"
+// let rollNumber= 101
+// let data={
+//     name,rollNumber
+// }
+// console.log(data.name)
+// console.log(data.rollNumber)
+
+
+// let f = "firstName"
+// let l = "lastName"
+
+// let data = {
+//     [f] : "Ronik",
+//     [l] : "Choudhary"
+// }
+// console.log(data.firstName);
+// console.log(data.lastName);
+
+
+// let f="first";
+// let l="last";
+
+// let data={
+//     [f] : "Ronik",
+//     [l] : "choudhary",
+
+
+// Show()
+// {
+//     console.log(this.first);
+//     console.log(this.last);
+// }
+// };
+// data.show();
+
+
+
+let f = "first";
+let l = "last";
+
+let data = {
+    [f]: "Ronik",
+    [l]: "choudhary",
+    
+    show() {
+        console.log(this.first);
+        console.log(this.last);
+    }
+};
+
+data.show();
